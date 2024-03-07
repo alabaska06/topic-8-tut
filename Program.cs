77e5fe87b6bb7c85f1fa23712a3c61f021e578d5
Alanna Smith <smitalan240@gmail.com>
2024-03-07 14:23:04 -0500

    gg

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace topic_8_tut
+﻿using System.Xml.Linq;
+
+namespace topic_8_tut
 {
     internal class Program
     {
@@ -15,19 +17,20 @@
             Console.WriteLine(name[2] + name.Substring(4, 4));
 
             char.ToUpper(name[0]);
-            (name[0] + "").ToUpper();
+            
             name[0].ToString().ToUpper();
             name.Substring(0, 1).ToUpper();
             //all of these take the first letter and capatalize it 
             */
 
-            string firstName, lastName, studentNum;
+            string firstName, lastName, studentNum, reverseName, backwards;
+
             Console.WriteLine("What is your first name?");
             firstName = Console.ReadLine();
-            firstName[0].ToString().ToUpper();
+
             Console.WriteLine("What is your last name?");
             lastName = Console.ReadLine();
-            lastName[0].ToString().ToUpper();
+
             Console.WriteLine("What is your student number?");
             studentNum = Console.ReadLine();
 
@@ -37,6 +40,45 @@
             Console.WriteLine("Email:");
             Console.WriteLine(firstName[0] + lastName + "@parkside.com");
 
+            Console.WriteLine($"{firstName.ToUpper()} {lastName.ToLower()}");
+            reverseName = (firstName + lastName);
+            Console.WriteLine(reverseName);
+            backwards = "";
+            for (int i = reverseName.Length - 1; i >= 0; i--)
+            {
+                backwards = backwards + reverseName.Substring(i, 1);
+            }
+
+            backwards = "";
+
+            for (int i = 0; i < reverseName.Length; i++)
+            {
+                backwards = reverseName.Substring(i, 1) + backwards;
+            }
+
+            Console.WriteLine(backwards);
+
+            
+            /*
+            string name1 = "jordyna";
+            string name2 = "Jordyna";
+            if (name1.Equals(name2, StringComparison.OrdinalIgnoreCase))
+                Console.WriteLine("Same");
+            else
+                Console.WriteLine("Different");
+
+            string words = "Tom and Jerry";
+            if (words.Contains("and"))
+            {
+                Console.WriteLine("The phrase contains and.");
+            }
+            else
+            {
+                Console.WriteLine("The phrase does not contain and.");
+            }
+            */
+
+            
 
         } 
        
